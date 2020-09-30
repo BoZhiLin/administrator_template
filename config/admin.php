610a05c14528c5bf720329fab17c93df81cd6c9b
bozhilin <bozhi0925@gmail.com>
2020-09-30 17:43:00 +0800

    initial commit

---
+++ config/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'route' => [
+        'prefix' => env('ADMIN_ROUTE_PREFIX', 'admin'),
+
+    ],
+
+    'database' => [
+        'models' => [
+            
+        ],
+    ],
+
+];
