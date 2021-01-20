da543a7b066e5be67ea84c0660563d4049f1abe4
poopoopai <sword61040767@gmail.com>
2021-01-20 16:12:53 +0800

    [dev] 後台登入畫面

--- resources/backend/js/router/index.js
+++ resources/backend/js/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Login from '../views/Login';
+import Dashboard from '../views/dashboard/index.vue'
 
 Vue.use(VueRouter);
 
@@ -11,7 +12,12 @@ const router = new VueRouter({
       path: '/admin/login',
       name: 'admin.login',
       component: Login
-    }
+    },
+    {
+      path: '/admin/dashboard',
+      name: 'admin.dashboard',
+      component: Dashboard
+    },
   ]
 });
 
--- resources/backend/js/views/Login.vue
+++ resources/backend/js/views/Login.vue
@@ -1,21 +1,93 @@
 <template>
-  <b-container fluid class="bg-img1 bg-img">
-    <div>
-      登入頁
-    </div>
-  </b-container>
+  <div>
+  <b-card 
+    bg-variant="light"
+    header="Login"
+    class="text-center"
+    style="max-width: 25rem;"
+  >
+    <b-form-group
+      label="Username:"
+      label-for="username"
+      label-cols-sm="3"
+      label-align-sm="right"
+    >
+      <b-form-input 
+        id="username"
+        name="username"
+        placeholder="Enter your username"
+        v-model="username"
+      ></b-form-input>
+    </b-form-group>
+
+      <b-form-group
+        label="Password:"
+        label-for="password"
+        label-cols-sm="3"
+        label-align-sm="right"
+      >
+        <b-form-input 
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          v-model="password"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" @click="adminLogin" variant="primary" block >Login</b-button>
+  </b-card>
+</div>
 </template>
 
-
 <script>
+import axios from 'axios';
+import router from '../router/index'
+
 export default {
-  data() {
+  name: "Login",
+  data: function() {
     return {
-      // 
+        username: "",
+        password: "",
     }
   },
   methods: {
-    // 
+    adminLogin : function () {
+      axios.post('/admin/api/auth/login', {
+      username: this.username,
+      password: this.password
+      })
+      .then(function (response) {
+        const success = response.data;
+        const credential = success.data.credential;
+        
+        if (success.status === 0) {
+              localStorage.setItem("access_token", credential.access_token);
+              localStorage.setItem("expired_at", credential.expired_at);
+              router.push({ path: "/admin/dashboard" });
+            }
+        })
+      .catch(function (error) {
+        alert('Error');
+      });
+    }
   },
-}
+};
 </script>
+
+<style lang="scss">
+$light_gray:rgb(17, 8, 8);
+
+  .card {
+    position: absolute;;
+    margin: 200px auto;
+  }
+  .card-header {
+    font-size: 26px;
+    color: $light_gray;
+    text-align: center;
+    font-weight: bold;
+  }
+  .col-form-label {
+    font-weight: bold;
+  }
+</style>
